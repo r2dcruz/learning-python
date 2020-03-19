917ad309b6182d90270a7f163f0464c8f6747bde
Romaine De La Cruz <6153680+r2dcruz@users.noreply.github.com>
2020-03-18 22:48:56 -0700

    testing file

---
+++ basics.py
@@ -0,0 +1,12 @@
+#adding basic file through github. trying out the github way
+
+import os
+import sys
+
+def  my_function(one, two,
+                 three, four,
+                 five, six):
+    print("first python function ever")
+    
+    
+my_function();
